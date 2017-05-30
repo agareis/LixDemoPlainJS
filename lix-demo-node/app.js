e54e696da9cea8320df1282213faf0835f8b8a76
agareis <arturgareis@Arturs-MacBook.fritz.box>
2017-05-30 11:39:59 +0200

    separate module 'readability.js' created (issue #11 )
    1. computeLix function to compute the readability of a text exported

--- lix-demo-node/app.js
+++ lix-demo-node/app.js
@@ -4,49 +4,10 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-function getWordsCount(str) {
-  console.log("getWordsCount:" + str.split(' ').length);
-  return str.split(' ').length;
-}
-
-function getPeriodsCount(str) {
-    var dots = str.split(".").length - 1;
-    var colons = str.split(":").length - 1;
-    var capLetters = str.length - str.replace(/[A-Z]/g, '').length;
-    console.log("getPeriodsCount: ->dots: " +dots+" colons: "+ colons+" capLetters: "+capLetters);
-    return (dots+colons+capLetters);
-}
-function getLongWordsCount(str) {
-  var match = str.match(/[\w0-9]{7,}/gi);
-  if (match === null) {  // No long words were found
-    console.log("getLongWordsCount: "+ 0);
-    return 0;
-  }
-  console.log("getLongWordsCount: "+ match.length);
-  return match.length;
-}
-function computeLix(str) {
-  var lix = (getWordsCount(str) / getPeriodsCount(str)) +
-  (getLongWordsCount(str) * 100 / getWordsCount(str));
-  console.log("computeLix: "+ lix);
-
-  if (lix==Infinity) {
-    return "Not enought characters or periods. Please add or type some more content.";
-  } else if (lix < 30) {
-    return "Difficulty: Very easy";
-  } else if (lix>=30 && lix<40) {
-    return "Difficulty: Easy";
-  } else if (lix>=40 && lix<50) {
-    return "Difficulty: Medium";
-  } else if (lix>=50 && lix<60) {
-    return "Difficulty: Difficult";
-  } else if (lix>=60) {
-    return "Difficulty: Very Difficult";
-  }
-}
 // POST: {"text":"This is a Test"}  <-- JSON encoding
 app.post('/api/v1/readability', function (req, res) {
   var userText = req.body.text;
+  var computeLix = require('./readability.js')
   res.send(computeLix(userText));
 })
 
