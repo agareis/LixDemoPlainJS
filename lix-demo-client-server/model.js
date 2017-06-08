ce1877c079134b1816a5c9014d8ae919d281a468
agareis <arturgareis@Arturs-MacBook.fritz.box>
2017-06-08 15:26:18 +0200

    cars problem solved, still can't fetch() (issue #12 )
    Console error message:
    Uncaught (in promise) TypeError: Failed to execute 'fetch' on 'Window':
    1 argument required, but only 0 present.
        at HTMLFormElement.oninput (index.html:11)

--- lix-demo-client-server/model.js
+++ lix-demo-client-server/model.js
@@ -1,26 +1,26 @@
+
 const url = 'http://localhost:3000/api/v1/readability';
-// The data we are going to send in our request
-let data = {
-    text: "This is a Test."
-}
-// Create our request constructor with all the parameters we need
-var request = new Request(url, {
-    method: 'POST',
-     mode: 'no-cors',
-    body: data
-});
 
 
-fetch(request)
-//   'http://localhost:3000/api/v1/readability', {
-// 	method: 'post',
-//   mode: 'no-cors',
-// 	body: JSON.stringify({
-// 		text: document.getElementById('userText').value
-// 	})
-// })
-.then(function() {
+ fetch(url, {
+	method: 'post',
+  headers: {
+    'Content-Type': 'text/plain',
+    'Content-Type': 'application/json'
+  },
+  mode: 'cors',
+	body: JSON.stringify({
+		text: document.getElementById('userText').value
+	})
+})
+.then(function(res) {
     // Your code for handling the data you get from the API
+  console.log(res.ok);
+  console.log(res.status);
+  console.log(res.statusText);
+  // console.log(res.text());
+  console.log(res.headers.get('content-type'));
+  return res.text();
 }).catch(function(err) {
 	console.log('Fetch Error :-S', err);
 });
